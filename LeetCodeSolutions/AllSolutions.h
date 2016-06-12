1c7be737ba943030b9264645d986f602c7c2e5ad
Himangshu Ranjan Borah <himangshu.nits@gmail.com>
2016-06-11 21:15:14 -0400

    Initial Check In of LeetCode Solutions.

---
+++ LeetCodeSolutions/AllSolutions.h
@@ -0,0 +1,13 @@
+//
+//  AllSolutions.h
+//  LeetCodeSolutions
+//
+//  Created by Himangshu Ranjan Borah on 6/11/16.
+//  Copyright © 2016 NC State University. All rights reserved.
+//
+
+#ifndef AllSolutions_h
+#define AllSolutions_h
+
+
+#endif /* AllSolutions_h */
---
+++ LeetCodeSolutions/StringReverse.cpp
@@ -0,0 +1,31 @@
+//
+//  StringReverse.cpp
+//  LeetCodeSolutions
+//
+//  Created by Himangshu Ranjan Borah on 6/11/16.
+//  Copyright © 2016 NC State University. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string>
+#include "AllSolutions.h"
+
+using namespace std;
+
+class StringReverse{
+public:
+    StringReverse(string str){
+        _str = str;
+    }
+    
+    string GetReversedString(){
+        string result = "";
+        
+        
+        return result;
+    }
+    
+private:
+    string _str;
+
+};
