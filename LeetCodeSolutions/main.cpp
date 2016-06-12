a159d19e91ce247cb33382150d223b03d1788b17
Himangshu Ranjan Borah <himangshu.nits@gmail.com>
2016-06-11 21:08:14 -0400

    Initial Commit

---
+++ LeetCodeSolutions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCodeSolutions
+//
+//  Created by Himangshu Ranjan Borah on 6/11/16.
+//  Copyright © 2016 NC State University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
